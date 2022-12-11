5ae1a405f17b23dab128db80cc242632f0e6ecca
João Vidal <dev.joaovidal@gmail.com>
2022-12-11 16:07:16 -0300

    concatenação

---
+++ DIO/01-conhecendo_a_plataforma/13-manipulando_valores/formatando_valores_monetários/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+/* seta a configuração de moeda como en-US (Dólar) 
+ou seja: $1,582.70
+
+obs: essa configuração trava a cultura para todo o sistema, enquanto o default
+se adapta de acordo com o sistema do computador de acesso.
+*/
+
+
+decimal valorMonetario2 = 1582.70M; 
+/* o formatador vai corrigir ponto e virgular para a moeda real 
+ou seja: R$ 1.582,70
+*/
+
+Console.WriteLine($"{valorMonetario2:C}"); 
+/* :C formata para o padrão da moeda de acordo com o sistema */
+/* ":" significa que você quer formatar a string | "C" significa Currency (Moeda)*/
+
