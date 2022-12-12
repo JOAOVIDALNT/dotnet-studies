b4079cee73d828add6edaf98943f32f61cfccf63
João Vidal <dev.joaovidal@gmail.com>
2022-12-12 20:39:15 -0300

    culture info and formating percentage

--- DIO/01-conhecendo_a_plataforma/13-manipulando_valores/formatando_valores_monetários/Program.cs
+++ DIO/01-conhecendo_a_plataforma/13-manipulando_valores/formatando_valores_monetários/Program.cs
@@ -18,3 +18,19 @@ Console.WriteLine($"{valorMonetario2:C}");
 /* :C formata para o padrão da moeda de acordo com o sistema */
 /* ":" significa que você quer formatar a string | "C" significa Currency (Moeda)*/
 
+Console.WriteLine(valorMonetario2.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+/* Aplica uma cultura específica independente da cultura do sistema */
+
+Console.WriteLine(valorMonetario2.ToString("C3")); 
+/* 
+formata de acordo com o sistema e o número corresponde as casas decimais 
+"N4" por exemplo não exibiria o sinal de dólar antes do número e possuiria 4 casas decimais
+*/
+
+double porcentagem = .3421;
+
+Console.WriteLine(porcentagem.ToString("P")); // exibe a porcentagem 34.210% ou 34,21% (pt-BR)
+
+int numero = 123456;
+
+Console.WriteLine(numero.ToString("##-##-##")); // exibe 12-34-56
