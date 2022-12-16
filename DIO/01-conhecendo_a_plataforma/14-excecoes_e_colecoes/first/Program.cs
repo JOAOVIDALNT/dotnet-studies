7cfa3792e73d41403f88c4698b9833920c87bdfa
João Vidal <dev.joaovidal@gmail.com>
2022-12-16 20:57:45 -0300

    generic exception handling

--- DIO/01-conhecendo_a_plataforma/14-excecoes_e_colecoes/first/Program.cs
+++ DIO/01-conhecendo_a_plataforma/14-excecoes_e_colecoes/first/Program.cs
@@ -1,6 +1,15 @@
-﻿string[] lines = File.ReadAllLines("Archives/letter.txt");
+﻿try 
+{
+string[] lines = File.ReadAllLines("Archives/letter1.txt"); // file reader do c#
 
 foreach(string line in lines) 
 {
     Console.WriteLine(line);
 }
+
+} catch(Exception e) 
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.WriteLine("Chegou até aqui");
