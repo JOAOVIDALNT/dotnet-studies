055e0cc0e2050c3b0b9467fffde5a3be74ac8a0b
João Vidal <dev.joaovidal@gmail.com>
2022-12-16 22:17:43 -0300

    queue introduction

---
+++ DIO/01-conhecendo_a_plataforma/14-excecoes_e_colecoes/fila/Program.cs
@@ -0,0 +1,21 @@
+﻿Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
+
+// fila.Dequeue(); -> sempre remove o primeiro elemento 
+
+Console.WriteLine($"Removendo o elemento {fila.Dequeue()}");
+fila.Enqueue(10);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
