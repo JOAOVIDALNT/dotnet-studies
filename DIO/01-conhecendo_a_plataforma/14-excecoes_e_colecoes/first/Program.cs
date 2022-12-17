6d679bbaca8fd1fbdfa05f4b9a9bdfa61150d732
João Vidal <dev.joaovidal@gmail.com>
2022-12-16 21:13:55 -0300

    especific exception handling

--- DIO/01-conhecendo_a_plataforma/14-excecoes_e_colecoes/first/Program.cs
+++ DIO/01-conhecendo_a_plataforma/14-excecoes_e_colecoes/first/Program.cs
@@ -7,9 +7,21 @@ foreach(string line in lines)
     Console.WriteLine(line);
 }
 
-} catch(Exception e) 
+} 
+catch (FileNotFoundException e) 
+{
+    Console.WriteLine($"Arquivo não encontrado {e.Message}");
+} 
+catch (DirectoryNotFoundException e) 
+{
+    Console.WriteLine($"Caminho da pasta não encontrado {e.Message}");
+} 
+catch (Exception e)
 {
     Console.WriteLine(e.Message);
 }
+finally // independe de exceção ou não
+{
+    Console.WriteLine("Chegou até aqui");
+}
 
-Console.WriteLine("Chegou até aqui");
