2dff5408b7074f054387be33ec07e01d2abc92b7
João Vidal <dev.joaovidal@gmail.com>
2023-01-03 11:28:57 -0300

    generic clsses

---
+++ DIO/01-conhecendo_a_plataforma/17-tipos_especiais/anonymous-types_in_collections/Program.cs
@@ -0,0 +1,13 @@
+﻿using _02.Models;
+using Newtonsoft.Json;
+
+string fileContent = File.ReadAllText("File/sells.json");
+
+List<Product> sellList = JsonConvert.DeserializeObject<List<Product>>(fileContent);
+
+var anomList = sellList.Select(x => new { x.Name, x.Price}); // obtem através de um tipo anônimo apenas nome e preço 
+
+foreach (var sell in anomList)
+{
+    Console.WriteLine($"Product: {sell.Name}, Price: {sell.Price}");
+}
