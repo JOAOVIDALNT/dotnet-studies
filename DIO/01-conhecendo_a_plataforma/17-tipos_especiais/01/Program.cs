e8320ee2cdeaa9bdd256d28044d06656ba1eaaf9
João Vidal <dev.joaovidal@gmail.com>
2023-01-03 10:03:37 -0300

    nullable type introduction

---
+++ DIO/01-conhecendo_a_plataforma/17-tipos_especiais/01/Program.cs
@@ -0,0 +1,14 @@
+﻿    bool nonNullable = true; //recebe apenas os valores true ou false
+    bool? desejaReceberEmail = null; //recebe, além de true e false o valor null
+
+    // TIPOS NULLABtLE RECEBEM AS FUNÇÕES HASVALUE(checa se há valor) & VALUE (recebe o valor)
+    // ex:
+    if (desejaReceberEmail.HasValue && desejaReceberEmail.Value) 
+    // desejaReceberEmail != null && desejaReceberEmail.Value == true
+    {
+        Console.WriteLine("O usuário optou por receber o email");
+    }
+    else 
+    {
+        Console.WriteLine("o usuário não respondeu ou optou por não receber o email");
+    }
