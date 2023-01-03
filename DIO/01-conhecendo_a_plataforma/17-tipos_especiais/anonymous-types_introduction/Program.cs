d919f825dcf0e2110272a5d1cf12444242510c5f
João Vidal <dev.joaovidal@gmail.com>
2023-01-03 10:35:59 -0300

    anonymous types introduction

---
+++ DIO/01-conhecendo_a_plataforma/17-tipos_especiais/anonymous-types_introduction/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+var anomType = new{Nome = "João", Sobrenome = "Vidal", Idade = 22};
+
+Console.WriteLine($"Nome: {anomType.Nome}"); // Propriedade readonly (possui apenas o método get)
+Console.WriteLine($"Sobrenome: {anomType.Sobrenome}");
+Console.WriteLine($"Idade: {anomType.Idade}");
+
+// TIPO ANONIMO NÃO PODE SER RETORNADO EM MÉTODOS
