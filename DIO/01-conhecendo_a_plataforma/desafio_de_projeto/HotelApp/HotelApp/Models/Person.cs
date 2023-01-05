8f531ebe4c41cc382012f85d3918a63f9421c43b
João Vidal <dev.joaovidal@gmail.com>
2023-01-05 19:42:05 -0300

    project challenge

---
+++ DIO/01-conhecendo_a_plataforma/desafio_de_projeto/HotelApp/HotelApp/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace HotelApp.Models;
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public Person(string name, string surname)
+    {
+        Name = name;
+        Surname = surname;
+    }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public Person()
+    {
+    }
+
+    public string FullName => $"{Name} {Surname}".ToUpper();
+}
