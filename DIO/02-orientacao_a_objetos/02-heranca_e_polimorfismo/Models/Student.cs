c453a6c2f41fa56f5c8b6fe35b30742b7fba2daf
João Vidal <dev.joaovidal@gmail.com>
2023-01-09 19:43:42 -0300

    herança

---
+++ DIO/02-orientacao_a_objetos/02-heranca_e_polimorfismo/Models/Student.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02_heranca_e_polimorfismo.Models
+{
+    public class Student : Person
+    {
+        public double Grade { get; set; }
+    }
+}
---
+++ DIO/02-orientacao_a_objetos/02-heranca_e_polimorfismo/Program.cs
@@ -0,0 +1,8 @@
+﻿using _02_heranca_e_polimorfismo.Models;
+
+
+Student s1 = new Student();
+s1.Name = "João";
+s1.Age = 22;
+
+s1.Present();
