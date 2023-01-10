c23c45e3953243abd6443d781f53f1598515a16a
João Vidal <dev.joaovidal@gmail.com>
2023-01-09 21:50:48 -0300

    classe object

---
+++ DIO/02-orientacao_a_objetos/03-classes_abstratas_e_interfaces/classe_object/Models/Computer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace classe_object.Models
+{
+    public class Computer
+    {
+        public override string ToString()
+        {
+            // return base.ToString();
+            return "Método ToString sobreescrito";
+        }
+    }
+}
