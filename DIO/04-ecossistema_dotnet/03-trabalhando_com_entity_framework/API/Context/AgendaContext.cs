67e9bc06870ba534f2beeb8d0be0fdb2cdc5b264
João Vidal <dev.joaovidal@gmail.com>
2023-01-20 19:32:31 -0300

    creating context

---
+++ DIO/04-ecossistema_dotnet/03-trabalhando_com_entity_framework/API/Context/AgendaContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Context
+{
+    public class AgendaContext : DbContext
+    {
+        // ENTIDADE É UMA CLASSE E AO MESMO TEMPO UMA TABELA NO DB
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+            // RECEBE A CONEXÃO/CONFIGURAÇÃO DO BANCO ATRAVÉS DO options E PASSA PARA O base QUE É O DbContext POIS ELE É QUEM GERENCIA A CONEXÃO COM O BANCO DE DADOS
+            // SERVE APENAS COMO CONSTRUTOR. SEM CORPO.
+        }
+
+        public DbSet<Contato> Contatos {get; set;}
+        // Está dentro do dbset pois é uma classe no nosso programa e vai ser representado por uma tabela no banco de dados
+
+    }
+}
