3d992c94e22f500bcf30524f2bf28f38c0745562
João Vidal <dev.joaovidal@gmail.com>
2023-01-20 19:48:45 -0300

    config db connection

--- DIO/04-ecossistema_dotnet/03-trabalhando_com_entity_framework/API/Program.cs
+++ DIO/04-ecossistema_dotnet/03-trabalhando_com_entity_framework/API/Program.cs
@@ -1,6 +1,20 @@
+using API.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+/* 
+Adiciona um dbcontext do tipo AgendaContext passando algumas opções:
+> options.UseSqlServer - indica o db a ser usado (poderia ser MySql por exemplo)
+> builder.Configuration - busca a configuração do appsettings.json
+> .GetConnectionString("") - busca dentro do appsettings pela conexão passada no parâmetro
+*/
+
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ DIO/04-ecossistema_dotnet/03-trabalhando_com_entity_framework/API/json_e_connection_string.md
@@ -0,0 +1,4 @@
+### appsettings e connection string
+
+- appsettings.Development.json é usado para ambiente de desenvolvimento enquanto
+appsettings.json é usualmente usado para ambiente de produção
