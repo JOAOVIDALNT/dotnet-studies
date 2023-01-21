fd4640bba0dc78bb4938200328bff53fa840cf5d
João Vidal <dev.joaovidal@gmail.com>
2023-01-21 14:24:19 -0300

    creating views

--- DIO/04-ecossistema_dotnet/04-front_end_com_asp.net_mvc/ProjetoMVC/Controllers/ContatoController.cs
+++ DIO/04-ecossistema_dotnet/04-front_end_com_asp.net_mvc/ProjetoMVC/Controllers/ContatoController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Context;
 
 namespace ProjetoMVC.Controllers
 {
     public class ContatoController : Controller // mvc é mais simples
     {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
         }
     }
 }
