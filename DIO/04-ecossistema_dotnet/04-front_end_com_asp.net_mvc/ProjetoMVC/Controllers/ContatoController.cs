1c3374a818c17d4faca1436c5530d6e4c7ce8fab
João Vidal <dev.joaovidal@gmail.com>
2023-01-23 20:01:31 -0300

    create details

--- DIO/04-ecossistema_dotnet/04-front_end_com_asp.net_mvc/ProjetoMVC/Controllers/ContatoController.cs
+++ DIO/04-ecossistema_dotnet/04-front_end_com_asp.net_mvc/ProjetoMVC/Controllers/ContatoController.cs
@@ -64,5 +64,15 @@ namespace ProjetoMVC.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
     }
 }
