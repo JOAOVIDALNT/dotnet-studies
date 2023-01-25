f9da18ff5ed601152073c0948b81a7a36fe03288
João Vidal <dev.joaovidal@gmail.com>
2023-01-24 22:37:07 -0300

    final challenge commited first at gitlab

---
+++ DIO/04-ecossistema_dotnet/desafio_final/WebApplication1/Models/Seller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models;
+
+public class Seller
+{
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Cpf { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
---
+++ DIO/04-ecossistema_dotnet/desafio_final/WebApplication1/Models/Status.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models;
+
+public enum Status
+
+{
+    AguardandoPagamento,
+    PagamentoAprovado,
+    Enviado,
+    Entregue,
+    Cancelado
+        
+}
