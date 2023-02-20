d78310a27b4b8f4f384b5a810f5b7bfd05e8f1ab
João Vidal <dev.joaovidal@gmail.com>
2023-02-19 23:14:32 -0300

    unit test

---
+++ concepts/tests/testes_unitarios/Calculator.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testes_unitarios
+{
+    public class Calculator
+    {
+        private CalculatorState _state = CalculatorState.Cleared;
+    }
+}
---
+++ concepts/tests/testes_unitarios/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace testes_unitarios;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
