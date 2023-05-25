d553eba44666f4f228ed69af8f5eb67949a1c5a0
João Vidal <dev.joaovidal@gmail.com>
2023-05-24 23:12:23 -0300

    start villa project

---
+++ courses/udemy/dotnet-api/project/Magic-Villa/Magic-Villa_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Magic_Villa_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+    }
+}
