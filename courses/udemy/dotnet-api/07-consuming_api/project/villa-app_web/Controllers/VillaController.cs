acf4a53375f2e8ca704b44835f104995b1b6a890
João Vidal <dev.joaovidal@gmail.com>
2023-06-19 22:51:41 -0300

    finish consume implementations at mvc

---
+++ courses/udemy/dotnet-api/07-consuming_api/project/villa-app_web/Controllers/VillaController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using villa_app_web.Models;
+using villa_app_web.Models.dto;
+using villa_app_web.Services.IServices;
+
+namespace villa_app_web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+
+        public VillaController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> list = new();
+
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
