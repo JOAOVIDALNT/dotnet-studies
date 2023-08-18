f5f947769f5611d34e23f3f9a305a6db5a396f47
João Vidal <dev.joaovidal@gmail.com>
2023-08-17 22:02:33 -0300

    finish api_consuming module

--- courses/udemy/dotnet-api/07-consuming_api/project/villa-app_web/Controllers/VillaController.cs
+++ courses/udemy/dotnet-api/07-consuming_api/project/villa-app_web/Controllers/VillaController.cs
@@ -46,9 +46,11 @@ namespace villa_app_web.Controllers
                 var response = await _villaService.CreateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Villa created successfully";
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
+            TempData["error"] = "Error encountered";
             return View(model);
         }
 
@@ -73,9 +75,11 @@ namespace villa_app_web.Controllers
                 var response = await _villaService.UpdateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Villa updated successfully";
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
+            TempData["error"] = "Error encountered";
             return View(model);
         }
 
@@ -98,9 +102,10 @@ namespace villa_app_web.Controllers
             var response = await _villaService.DeleteAsync<APIResponse>(model.Id);
             if (response != null && response.IsSuccess)
             {
+                TempData["success"] = "Villa deleted successfully";
                 return RedirectToAction(nameof(IndexVilla));
             }
-
+            TempData["error"] = "Error encountered";
             return View(model);
         }
     }
