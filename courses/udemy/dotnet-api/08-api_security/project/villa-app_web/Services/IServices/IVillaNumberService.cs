f3a4f5416aa784b5fbc65b1089ca0cd86964fdf6
João Vidal <dev.joaovidal@gmail.com>
2023-08-17 22:04:02 -0300

    add module 8 - api security project

---
+++ courses/udemy/dotnet-api/08-api_security/project/villa-app_web/Services/IServices/IVillaNumberService.cs
@@ -0,0 +1,14 @@
+﻿using villa_app_web.Models.Dtos;
+
+namespace villa_app_web.Services.IServices
+{
+    public interface IVillaNumberService
+    {
+        Task<T> GetAllAsync<T>();
+        Task<T> GetAsync<T>(int id);
+        Task<T> CreateAsync<T>(VillaNumberCreateDTO dto);
+        Task<T> UpdateAsync<T>(VillaNumberUpdateDTO dto);
+        Task<T> DeleteAsync<T>(int id);
+
+    }
+}
