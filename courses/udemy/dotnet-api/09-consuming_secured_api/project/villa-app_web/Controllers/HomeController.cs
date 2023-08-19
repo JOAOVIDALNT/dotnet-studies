06a14b80d7c226def34d0665b42ddbd2bec20355
João Vidal <dev.joaovidal@gmail.com>
2023-08-19 10:33:19 -0300

    add cosuming api security project

---
+++ courses/udemy/dotnet-api/09-consuming_secured_api/project/villa-app_web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using villa_app_web.Models.Dtos;
+using villa_app_web.Models.Entities;
+using villa_app_web.Services.IServices;
+
+namespace villa_app_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+
+        public HomeController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<VillaDTO> list = new();
+
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
---
+++ courses/udemy/dotnet-api/09-consuming_secured_api/project/villa-app_web/Services/VillaService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using villa_app_utility;
+using villa_app_web.Models.Entities;
+using villa_app_web.Models.Dtos;
+using villa_app_web.Services.IServices;
+
+namespace villa_app_web.Services
+{
+    public class VillaService : BaseService, IVillaService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private string villaUrl;
+        public VillaService(IHttpClientFactory httpClientFactory, IConfiguration configuration) : base(httpClientFactory)
+        {
+
+            _httpClientFactory = httpClientFactory;
+            villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+
+        }
+        public Task<T> CreateAsync<T>(VillaCreateDTO dto)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = Details.ApiType.POST,
+                Data = dto,
+                Url = villaUrl+"/api/villa-api/"
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = Details.ApiType.DELETE,
+                Url = villaUrl + "/api/villa-api/"+id
+            });
+        }
+
+        public Task<T> GetAllAsync<T>()
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = Details.ApiType.GET,
+                Url = villaUrl + "/api/villa-api/"
+            });
+        }
+
+        public Task<T> GetAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = Details.ApiType.GET,
+                Url = villaUrl + "/api/villa-api/"+id
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(VillaUpdateDTO dto)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = Details.ApiType.PUT,
+                Data = dto,
+                Url = villaUrl + "/api/villa-api/" + dto.Id
+            });
+        }
+    }
+}
