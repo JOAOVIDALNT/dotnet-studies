4753492d77929d8bc88821e654dceb941072a5b1
João Vidal <dev.joaovidal@gmail.com>
2023-08-20 19:07:08 -0300

    add 12 module folder

---
+++ courses/udemy/dotnet-api/12-dotnet_identity/project/villa-app_web/Models/Dtos/LoginRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace villa_app_web.Models.Dtos
+{
+    public class LoginRequestDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ courses/udemy/dotnet-api/12-dotnet_identity/project/villa-app_web/Services/BaseService.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using villa_app_utility;
+using villa_app_web.Models.Entities;
+using villa_app_web.Services.IServices;
+
+namespace villa_app_web.Services
+{
+    public class BaseService : IBaseService
+    {
+
+        public APIResponse responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+
+        public async Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("VillaAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+
+                if(apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                        Encoding.UTF8, "application/json");
+                }
+
+                switch(apiRequest.ApiType)
+                {
+                    case Details.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+
+                    case Details.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+
+                    case Details.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                HttpResponseMessage apiResponse = null;
+
+                if (!string.IsNullOrEmpty(apiRequest.Token))
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiRequest.Token);
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                try
+                {
+                    APIResponse ApiResponse = JsonConvert.DeserializeObject<APIResponse>(apiContent);
+                    if (ApiResponse != null && (apiResponse.StatusCode == System.Net.HttpStatusCode.BadRequest
+                        || apiResponse.StatusCode == System.Net.HttpStatusCode.NotFound))
+                    {
+                        ApiResponse.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                        ApiResponse.IsSuccess = false;
+
+                        var res = JsonConvert.SerializeObject(ApiResponse);
+                        var returnObj = JsonConvert.DeserializeObject<T>(res);
+
+                        return returnObj;
+                    }
+                }
+                catch
+                {
+                    var exceptionResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                    return exceptionResponse;
+                }
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                return APIResponse;
+
+            }
+                
+            catch (Exception ex)
+            {
+                var dto = new APIResponse
+                {
+                    ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+
+                var res = JsonConvert.SerializeObject(dto);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+
+                return APIResponse;
+            }
+        }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new();
+            this.httpClient = httpClient;
+
+        }
+    }
+}
