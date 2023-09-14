91e1dad608e8814e906a10ddd129e028d4b83d26
João Vidal <dev.joaovidal@gmail.com>
2023-09-13 21:59:54 -0300

    exclude unusable projects

--- projects/securityDemo/securityDemo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace securityDemo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
