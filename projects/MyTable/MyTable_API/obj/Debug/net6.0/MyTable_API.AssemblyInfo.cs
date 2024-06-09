2f4c453d08bce9ab375aa8df7ae108575caaeead
João Vidal <dev.joaovidal@gmail.com>
2024-06-09 14:20:46 -0300

    Unit test introduction

--- projects/MyTable/MyTable_API/obj/Debug/net6.0/MyTable_API.AssemblyInfo.cs
+++ projects/MyTable/MyTable_API/obj/Debug/net6.0/MyTable_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTable_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf943b7dfa919ea5c930ce7a88b06b1797e18205")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTable_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTable_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
