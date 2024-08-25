9970c8313b86c8902e4390ede5f31e937230df33
João Vidal <dev.joaovidal@gmail.com>
2024-08-25 19:15:03 -0300

    calc 100 - 200

---
+++ challenges/calc_aindamais_braba/calc_aindamais_braba/Program.cs
@@ -0,0 +1,17 @@
+﻿var soma = 0;
+
+for (int i = 101; i <= 200; i++)
+{
+    if (i.Mod())
+        soma += i;
+}
+
+Console.WriteLine(soma);
+
+public static class Extension
+{
+    public static bool Mod(this int num)
+    {
+        return num % 2 != 0;
+    }
+}
